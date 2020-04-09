40d0e106529f6e2be64eb05511ce7b091552c433
Alessandro Grossano <grossano.alessandro@gmail.com>
2020-04-09 20:47:27 +0200

    Terminato

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 $(document).ready(function () {
 
   //riferimenti html
-  var sendBtn = $('.send-msg__send i');//bottone invio
-  var textInput = $('.send-msg__input-box');//campo di testo invio messaggio
-  var searchInput = $('.search__input');//campo di ricerca contatti
-  var user = $('.user__msg')//blocco utente
-
+  var sendBtn = $('.send-msg__send i'); //bottone invio
+  var textInput = $('.send-msg__input-box'); //campo di testo invio messaggio
+  var searchInput = $('.search__input'); //campo di ricerca contatti
+  var user = $('.user__msg'); //blocco utente
 
   //funzione che posta messaggio e risposta
   function appendText() {
-    var msg = textInput.val() //valore campo input 
+    var msg = textInput.val(); //valore campo input 
     //se il campo input non è vuoto posta messaggio
     if (msg) {
       $(".window--active ").append('<div class="window__message message--out"><span>' + msg + '</span><i class="fa fa-chevron-down f-right message-options"></i><div class="message__panel"><span>Info messaggio</span><span class="delete">Elimina messaggio</span></div></div>');
-      //svuota campo input
-      textInput.val("");
+      textInput.val(""); //svuota campo input
       //funzione timeout che invia messaggio dopo 1 secondo
       setTimeout(function () {
         $(".window--active ").append('<div class="window__message message--in"><span>Ciao!</span><i class="fa fa-chevron-down f-right message-options"></i><div class="message__panel"><span>Info messaggio</span><span class="delete">Elimina messaggio</span></div></div>');
       }, 1000);
     }
   }
-
 
   //funzione invio messaggio da click
   $(sendBtn).click(function () {
@@ -35,14 +32,12 @@ $(document).ready(function () {
     }
   });
 
-
-  //funzione ricerca contatti in ascolto pressione tasti
+  //funzione ricerca contatti in ascolto su pressione tasti
   $(searchInput).keyup(function (e) {
     $.each(user, function () {
       //salvo il nome dell'user corrente e lo trasformo in lettere minuscole
-      var currentUser = $('.msg__user-name-last p', this).html().toLowerCase();
-      //se il nome dell'utente nell'iterazione corrente è uguale al valore di input digitato mostro il div corrispondente, altrimenti lo nascondo
-      if (currentUser.includes(e.target.value)) { //
+      var currentUser = $('.msg__user-name-last p', this).html().toLowerCase(); //se il nome dell'utente nell'iterazione corrente è uguale al valore di input digitato mostro il div corrispondente, altrimenti lo nascondo
+      if (currentUser.includes(e.target.value)) {
         $(this).show();
       } else {
         $(this).hide();
@@ -50,36 +45,27 @@ $(document).ready(function () {
     });
   });
 
-
   //funzione cambio chat
   $(".user__msg").click(function () {
-    //salvo index dell'user cliccato
-    var clickedUser = $(this).index();
-    //salvo riferimento al div dei messaggi
-    var chatList = $('.right__chat > div')
-    //rimuovo la classe window--active a tutti i div messaggi
-    chatList.removeClass("window--active")
-    //aggiungo la classe active all'user cliccato
-    $(this).addClass("msg--active")
-    //tolgo la classe active a tutti gli user ad eccetto di quello cliccato
-    $('.user__msg').not(this).removeClass("msg--active")
-    //aggiungo la classe active alla chat corrispondente l'user cliccato
-    chatList.eq(clickedUser).addClass("window--active");
+    var clickedUser = $(this).index(); //salvo index dell'user cliccato
+    var chatList = $('.right__chat > div'); //salvo riferimento al div dei messaggi
+    chatList.removeClass("window--active"); //rimuovo la classe window--active a tutti i div messaggi
+    $(this).addClass("msg--active"); //aggiungo la classe active all'user cliccato
+    $('.user__msg').not(this).removeClass("msg--active"); //tolgo la classe active a tutti gli user ad eccetto di quello cliccato
+    chatList.eq(clickedUser).addClass("window--active"); //aggiungo la classe active alla chat corrispondente l'user cliccato
   });
 
-  //funzione per rimuovere messaggio
+  //funzione rimozione messaggio
   $('.chat__window').on("click", ".message__panel",
     function () {
       $(this).parent().remove();
     })
-
 
   //funzione per mostrare / nascondere pannello messaggi
   $('.chat__window').on("click", ".window__message i",
     function () {
       $(this).siblings().toggleClass("panel--active");
     })
-
 
 });
 
